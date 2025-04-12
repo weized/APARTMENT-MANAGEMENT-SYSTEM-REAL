03a7ba4770365d34458fe3a88c2f0257d70213eb
dember marc <23-09786@g.batstate-u.edu.ph>
2025-04-12 21:25:54 +0800

    komit

--- src/main/java/com/oopfinals/OOP/buraticus.java
+++ src/main/java/com/oopfinals/OOP/buraticus.java
@@ -2,6 +2,6 @@ package com.oopfinals.OOP;
 
 public class buraticus {
     public static void main(String[] args) {
-        System.out.println("hello lord patawad");
+        System.out.println("hello world")
     }
 }
